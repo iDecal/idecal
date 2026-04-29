add257c9e86756ec61d9bb31d3715de26b253252
iDecal <105539236+iDecal@users.noreply.github.com>
2026-04-29 16:00:08 +0700

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,5 @@
 import { defineConfig } from 'astro/config';
-import cloudflare from '@astrojs/cloudflare';
-import tailwind from '@astrojs/tailwind'; // THÊM DÒNG NÀY
 
 export default defineConfig({
-  output: 'static',
-  adapter: cloudflare(),
-  integrations: [
-    tailwind(), 
-  ],
+  output: 'static'
 });
